114d2de354693587d2941a4aebdbf383c6f13437
tikho <tikhonsmovzh@gmail.com>
2024-08-31 22:19:19 +0300

    feat: start rewrite code
    final: PIDRegulator.kt and Angle.kt

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/linearOpModes/LinearOpModeBase.kt
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.linearOpModes
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import org.firstinspires.ftc.teamcode.utils.telemetry.StaticTelemetry
+
+class LinearOpModeBase: LinearOpMode() {
+    override fun runOpMode() {
+        StaticTelemetry.setPhoneTelemetry(telemetry)
+
+        try {
+            waitForStart()
+            resetRuntime()
+
+            while (opModeIsActive()) {
+                StaticTelemetry.update()
+            }
+        }
+        catch (e: Exception){
+            StaticTelemetry.addLine(e.message!!)
+
+            for (i in e.stackTrace)
+                StaticTelemetry.addLine(i.javaClass.`package`!!.name)
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/pidRegulator/PIDRegulator.kt
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.utils.pidRegulator
+
+import com.acmerobotics.roadrunner.clamp
+import com.qualcomm.robotcore.util.ElapsedTime
+
+data class PIDConfig(var p: Double, var i: Double = 0.0, var limitI: Double = 0.0, var d: Double = 0.0, var f: Double = 0.0, var g: Double = 0.0, var limitU: Double = 1.0)
+
+class PIDRegulator(val config: PIDConfig) {
+    private val _deltaTime = ElapsedTime()
+    private var _integral = 0.0
+    private var _errOld = 0.0
+
+    fun update(err: Double, target: Double = 0.0) : Double{
+        val uP = err * config.p
+
+        _integral += err * _deltaTime.seconds()
+        _integral = clamp(_integral, -config.limitI, config.limitI)
+        val uI = _integral * config.i
+
+        val uD = (err - _errOld) / _deltaTime.seconds() * config.d
+        _errOld = err
+
+        var u = uP + uI + uD + target * config.f + config.g
+        u = clamp(u, -config.limitU, config.limitU)
+
+        _deltaTime.reset()
+
+        return u
+    }
+}
