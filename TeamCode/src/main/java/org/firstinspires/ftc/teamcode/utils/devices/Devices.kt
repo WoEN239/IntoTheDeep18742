11a6889127baf2c421d0f0455c1b30511ce8e82c
tikho <tikhonsmovzh@gmail.com>
2024-09-01 18:41:51 +0300

    feat(Battery): добавлен класс батарейки для учета заряда
    он находится в Devices.battery

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/devices/Devices.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/devices/Devices.kt
@@ -2,7 +2,20 @@ package org.firstinspires.ftc.teamcode.utils.devices
 
 import com.qualcomm.robotcore.hardware.HardwareMap
 import com.qualcomm.robotcore.hardware.IMU
+import com.qualcomm.robotcore.hardware.VoltageSensor
+
+class Battery (private val _voltageSensor: VoltageSensor){
+    var charge = 1.0
+    var voltage = 1.0
+
+    fun update(){
+        voltage = _voltageSensor.voltage
+
+        charge = voltage / 13.0
+    }
+}
 
 class Devices(hardMap: HardwareMap)  {
     val imu = hardMap.get("imu") as IMU
+    val battery = Battery(hardMap.get("Control Hub") as VoltageSensor)
 }
