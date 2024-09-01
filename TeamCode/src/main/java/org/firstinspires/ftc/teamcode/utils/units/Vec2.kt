99fd1eab191882d2b893f28d90a439164951662f
tikho <tikhonsmovzh@gmail.com>
2024-09-01 10:54:16 +0300

    feat(Vec2): дописан класс вектора теперь у вектора перегружены базовые операции

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/units/Vec2.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/units/Vec2.kt
@@ -1,11 +1,24 @@
 package org.firstinspires.ftc.teamcode.utils.units
 
+import kotlin.math.cos
+import kotlin.math.sin
 import kotlin.math.sqrt
 
-class Vec2 (var X: Double, var Y: Double) {
+data class Vec2(var x: Double, var y: Double) {
     val ZERO = Vec2(0.0, 0.0)
 
-    fun length() = X * X + Y * Y
+    fun length() = x * x + y * y
 
     fun sqrtLength() = sqrt(length())
+
+    fun setRot(rot: Double) : Vec2 {
+        val l = sqrtLength()
+
+        return Vec2(cos(rot) * l, sin(rot) * l)
+    }
+
+    operator fun plus(vec: Vec2) = Vec2(x + vec.x, y + vec.y)
+    operator fun minus(vec: Vec2) = Vec2(x - vec.x, y - vec.y)
+    operator fun times(vec: Vec2) = Vec2(x * vec.x, y * vec.y)
+    operator fun div(vec: Vec2) = Vec2(x / vec.x, y / vec.y)
 }
