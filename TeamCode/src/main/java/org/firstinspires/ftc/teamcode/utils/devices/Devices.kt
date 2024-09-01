405c8d5a356e476fe589eede5f2229d8878ea8a2
tikho <tikhonsmovzh@gmail.com>
2024-09-01 11:17:22 +0300

    feat(Devices): исправлен класс Устройств Я сомог его облегчить и вроде в предыдущем коммите была ошибка

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/devices/Devices.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/devices/Devices.kt
@@ -3,10 +3,6 @@ package org.firstinspires.ftc.teamcode.utils.devices
 import com.qualcomm.robotcore.hardware.HardwareMap
 import com.qualcomm.robotcore.hardware.IMU
 
-class Devices {
-    lateinit var imu: IMU
-
-    fun initHardware(hardMap: HardwareMap){
-        imu = hardMap.get("imu") as IMU
-    }
+class Devices(hardMap: HardwareMap)  {
+    val imu = hardMap.get("imu") as IMU
 }
