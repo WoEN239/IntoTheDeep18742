12a5e6297747d821aa99f3fed7cd99aa79bd8c00
tikho <tikhonsmovzh@gmail.com>
2024-09-01 16:51:33 +0300

    feat(Event, AcceptEvent): добавил евенты

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/collectors/BaseCollector.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/collectors/BaseCollector.kt
@@ -1,43 +1,41 @@
 package org.firstinspires.ftc.teamcode.collectors
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
-import org.firstinspires.ftc.teamcode.modules.driveTrain
 import org.firstinspires.ftc.teamcode.utils.devices.Devices
-
-class BaseCollector (val robot: LinearOpMode) {
+class BaseCollector(val robot: LinearOpMode) {
     val devices = Devices(robot.hardwareMap)
 
-    private val allModules: Array<IRobotModule> = arrayOf()
+    private val _allModules: Array<IRobotModule> = arrayOf()
 
-    fun init(){
-        for(i in allModules)
+    fun init() {
+        for (i in _allModules)
             i.init(this)
 
-        for(i in allModules)
+        for (i in _allModules)
             i.lateInit(this)
     }
 
-    fun start(){
-        for(i in allModules)
+    fun start() {
+        for (i in _allModules)
             i.start()
 
-        for(i in allModules)
+        for (i in _allModules)
             i.lateStart()
     }
 
-    fun update(){
-        for(i in allModules)
+    fun update() {
+        for (i in _allModules)
             i.update()
 
-        for(i in allModules)
+        for (i in _allModules)
             i.lateUpdate()
     }
 
-    fun stop(){
-        for(i in allModules)
+    fun stop() {
+        for (i in _allModules)
             i.stop()
 
-        for(i in allModules)
+        for (i in _allModules)
             i.lateStop()
     }
 }
