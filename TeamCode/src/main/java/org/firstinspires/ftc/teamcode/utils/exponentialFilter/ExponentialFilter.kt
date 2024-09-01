3edf2489aaff4d6e040d07859007a494833aa22f
tikho <tikhonsmovzh@gmail.com>
2024-09-01 18:32:30 +0300

    feat(ExponentialFilter): класс Экспоненциального фильтра

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/exponentialFilter/ExponentialFilter.kt
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.utils.exponentialFilter
+
+import com.qualcomm.robotcore.util.ElapsedTime
+import org.firstinspires.ftc.teamcode.utils.updateListener.IHandler
+import org.firstinspires.ftc.teamcode.utils.updateListener.UpdateHandler
+
+class ExponentialFilter(var coef: Double) : IHandler {
+    private val _deltaTime = ElapsedTime()
+
+    init {
+        UpdateHandler.addHandler(this)
+    }
+
+    override fun start() {
+        _deltaTime.reset()
+    }
+
+    fun updateRaw(value: Double, delta: Double): Double {
+        val result = value + delta * (_deltaTime.seconds() / (coef + _deltaTime.seconds()))
+
+        _deltaTime.reset()
+
+        return result
+    }
+
+    fun update(val1: Double, val2: Double): Double {
+        return updateRaw(val1, val1 - val2)
+    }
+}
