e576f102df76b61a5761195d0487ac3b8cd7efc1
tikhonsmovzh <81905988+tikhonsmovzh@users.noreply.github.com>
2024-09-21 20:03:19 +0300

    fix(EncoderController): добавил зануление жнкодера

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/motor/EncoderController.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/motor/EncoderController.kt
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.utils.motor
 
+import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.DcMotorEx
 import com.qualcomm.robotcore.util.ElapsedTime
 import org.firstinspires.ftc.teamcode.utils.configs.Configs
@@ -33,6 +34,9 @@ class EncoderController(val encoder: DcMotorEx) : IHandler {
     }
 
     override fun start() {
+        encoder.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+        encoder.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+
         _deltaTime.reset()
         _oldPosition = position
     }
