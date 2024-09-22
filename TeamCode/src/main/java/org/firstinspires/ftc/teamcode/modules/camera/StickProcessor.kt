2ed754005fac8174533c6fca54d88bdac2e57068
tikhonsmovzh <81905988+tikhonsmovzh@users.noreply.github.com>
2024-09-22 18:41:01 +0300

    fix(StickProcessor): фикс архитектуры

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/camera/StickProcessor.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/camera/StickProcessor.kt
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode.modules.camera
 
 import android.graphics.Bitmap
 import android.graphics.Canvas
-import android.os.Environment
 import org.firstinspires.ftc.robotcore.external.function.Consumer
 import org.firstinspires.ftc.robotcore.external.function.Continuation
 import org.firstinspires.ftc.robotcore.external.stream.CameraStreamSource
@@ -16,16 +15,13 @@ import org.opencv.core.Mat
 import org.opencv.core.MatOfPoint
 import org.opencv.core.MatOfPoint2f
 import org.opencv.core.Point
-import org.opencv.core.Rect
+import org.opencv.core.RotatedRect
 import org.opencv.core.Scalar
 import org.opencv.core.Size
-import org.opencv.imgcodecs.Imgcodecs.imread
 import org.opencv.imgproc.Imgproc.CHAIN_APPROX_SIMPLE
-import org.opencv.imgproc.Imgproc.COLOR_BGR2RGB
 import org.opencv.imgproc.Imgproc.COLOR_RGB2HSV
 import org.opencv.imgproc.Imgproc.MORPH_ERODE
 import org.opencv.imgproc.Imgproc.RETR_TREE
-import org.opencv.imgproc.Imgproc.arrowedLine
 import org.opencv.imgproc.Imgproc.blur
 import org.opencv.imgproc.Imgproc.cvtColor
 import org.opencv.imgproc.Imgproc.dilate
@@ -35,15 +31,18 @@ import org.opencv.imgproc.Imgproc.getStructuringElement
 import org.opencv.imgproc.Imgproc.line
 import org.opencv.imgproc.Imgproc.minAreaRect
 import org.opencv.imgproc.Imgproc.putText
-import org.opencv.imgproc.Imgproc.rectangle
-import org.opencv.imgproc.Imgproc.resize
-import java.lang.System.out
+import java.util.concurrent.Callable
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 import java.util.concurrent.atomic.AtomicReference
 
 
 class StickProcessor : VisionProcessor, CameraStreamSource {
     private var lastFrame: AtomicReference<Bitmap> =
         AtomicReference(Bitmap.createBitmap(1, 1, Bitmap.Config.RGB_565))
+
+    private val _executorService: ExecutorService =
+        Executors.newWorkStealingPool(Configs.CameraConfig.DETECT_THREADS_COUNT)
 
     override fun init(width: Int, height: Int, calibration: CameraCalibration?) {
 
@@ -60,59 +59,13 @@ class StickProcessor : VisionProcessor, CameraStreamSource {
         blur(hsvFrame, hsvFrame, Size(10.0, 10.0))
         cvtColor(hsvFrame, hsvFrame, COLOR_RGB2HSV)
 
-        val blueBinaryFrame = hsvFrame.clone()
+        val blueRects = detect(Configs.CameraConfig.BLUE_STICK_DETECT, hsvFrame.clone())
 
-        inRange(
-            blueBinaryFrame,
-            Scalar(
-                Configs.CameraConfig.BLUE_H_MIN,
-                Configs.CameraConfig.BLUE_S_MIN,
-                Configs.CameraConfig.BLUE_V_MIN
-            ),
-            Scalar(
-                Configs.CameraConfig.BLUE_H_MAX,
-                Configs.CameraConfig.BLUE_S_MAX,
-                Configs.CameraConfig.BLUE_V_MAX
-            ),
-            blueBinaryFrame
-        )
+        drawRotatedRects(drawFrame, blueRects, Scalar(0.0, 0.0, 255.0), Scalar(0.0, 255.0, 0.0))
 
-        erodeDilate(blueBinaryFrame, Configs.CameraConfig.ERODE_DILATE_BLUE)
+        val redRects = detect(Configs.CameraConfig.RED_STICK_DETECT, hsvFrame.clone())
 
-        erode(blueBinaryFrame, blueBinaryFrame, getStructuringElement(MORPH_ERODE, Size(Configs.CameraConfig.PRECOMPRESSION_BLUE, Configs.CameraConfig.PRECOMPRESSION_BLUE)))
-
-        dilateErode(blueBinaryFrame, Configs.CameraConfig.DILATE_ERODE_BLUE)
-
-        dilate(blueBinaryFrame, blueBinaryFrame, getStructuringElement(MORPH_ERODE, Size(Configs.CameraConfig.PRECOMPRESSION_BLUE, Configs.CameraConfig.PRECOMPRESSION_BLUE)))
-
-        val contours = arrayListOf<MatOfPoint>()
-
-        findContours(blueBinaryFrame, contours, Mat(), RETR_TREE, CHAIN_APPROX_SIMPLE)
-
-        for(i in contours){
-            val points = MatOfPoint2f()
-
-            points.fromArray(*i.toArray())
-
-            val rect = minAreaRect(points)
-
-
-
-            if(rect.size.height * rect.size.width > Configs.CameraConfig.MIN_STICK_AREA) {
-                //rectangle(drawFrame, rect.boundingRect(), Scalar(0.0, 0.0, 255.0), 5)
-
-                val points = Array<Point?>(4){null}
-
-                rect.points(points)
-
-                line(drawFrame, points[0], points[1], Scalar(0.0, 0.0, 255.0), 5)
-                line(drawFrame, points[1], points[2], Scalar(0.0, 0.0, 255.0), 5)
-                line(drawFrame, points[2], points[3], Scalar(0.0, 0.0, 255.0), 5)
-                line(drawFrame, points[3], points[0], Scalar(0.0, 0.0, 255.0), 5)
-
-                putText(drawFrame, rect.angle.toInt().toString(), rect.center, 5, 2.0, Scalar(0.0, 255.0, 0.0))
-            }
-        }
+        drawRotatedRects(drawFrame, redRects, Scalar(255.0, 0.0, 0.0), Scalar(0.0, 255.0, 0.0))
 
         val b = Bitmap.createBitmap(
             drawFrame.width(),
@@ -123,6 +76,88 @@ class StickProcessor : VisionProcessor, CameraStreamSource {
         lastFrame.set(b)
 
         return frame
+    }
+
+    private fun detect(
+        parameters: Configs.CameraConfig.StickDetectConfig,
+        hsvFrame: Mat
+    ): Collection<RotatedRect> {
+        inRange(
+            hsvFrame,
+            Scalar(parameters.H_MIN, parameters.S_MIN, parameters.V_MIN),
+            Scalar(parameters.H_MAX, parameters.S_MAX, parameters.V_MAX),
+            hsvFrame
+        )
+
+        erodeDilate(hsvFrame, parameters.ERODE_DILATE)
+
+        erode(
+            hsvFrame,
+            hsvFrame,
+            getStructuringElement(
+                MORPH_ERODE,
+                Size(parameters.PRECOMPRESSION, parameters.PRECOMPRESSION)
+            )
+        )
+
+        dilateErode(hsvFrame, parameters.DILATE_ERODE)
+
+        dilate(
+            hsvFrame,
+            hsvFrame,
+            getStructuringElement(
+                MORPH_ERODE,
+                Size(parameters.PRECOMPRESSION, parameters.PRECOMPRESSION)
+            )
+        )
+
+        val contours = arrayListOf<MatOfPoint>()
+
+        findContours(hsvFrame, contours, Mat(), RETR_TREE, CHAIN_APPROX_SIMPLE)
+
+        val tasks = arrayListOf<Callable<RotatedRect?>>()
+
+        for (i in contours)
+            tasks.add { processContour(i) }
+
+        val result = _executorService.invokeAll(tasks)
+
+        return result.mapNotNull { it.get() }
+    }
+
+    fun drawRotatedRects(mat: Mat, rects: Collection<RotatedRect>, rectColor: Scalar, textColor: Scalar) {
+        for (i in rects) {
+            val points = Array<Point?>(4) { null }
+
+            i.points(points)
+
+            line(mat, points[0], points[1], rectColor, 5)
+            line(mat, points[1], points[2], rectColor, 5)
+            line(mat, points[2], points[3], rectColor, 5)
+            line(mat, points[3], points[0], rectColor, 5)
+
+            putText(
+                mat,
+                i.angle.toInt().toString(),
+                i.center,
+                5,
+                2.0,
+                textColor
+            )
+        }
+    }
+
+    private fun processContour(contour: MatOfPoint): RotatedRect? {
+        val points = MatOfPoint2f()
+
+        points.fromArray(*contour.toArray())
+
+        val rect = minAreaRect(points)
+
+        if (rect.size.height * rect.size.width > Configs.CameraConfig.MIN_STICK_AREA)
+            return rect
+
+        return null
     }
 
     override fun onDrawFrame(
