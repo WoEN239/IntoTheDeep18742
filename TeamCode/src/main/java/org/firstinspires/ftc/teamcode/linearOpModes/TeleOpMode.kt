a1bd3b932c80f0ff48877d4ed196c003fc18be07
tikhonsmovzh <81905988+tikhonsmovzh@users.noreply.github.com>
2024-10-13 14:48:25 +0300

    feat(LinearOpModeBase): автоинит и страт телеопа

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/linearOpModes/TeleOpMode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/linearOpModes/TeleOpMode.kt
@@ -3,16 +3,14 @@ package org.firstinspires.ftc.teamcode.linearOpModes
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import org.firstinspires.ftc.teamcode.collectors.BaseCollector
 
-/**
- * @see LinearOpModeBase
- */
-
 @TeleOp
-class TeleOp: LinearOpModeBase() {
+class TeleOpMode: LinearOpModeBase() {
+    override fun getOpModeSettings() = OpModeSettings(isAutoStart = true, isPreInit = false)
+
     override fun getCollector(): BaseCollector{
         val collector = BaseCollector(this)
 
-        collector.addAdditionalModules(arrayOf(/*ся модули*/))
+        collector.addAdditionalModules(arrayOf(/*ся модули для телеопа*/))
 
         return collector
     }
