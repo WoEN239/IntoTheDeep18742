46880164a20085a7743205dfc2b4e3685721cff4
tikhonsmovzh <81905988+tikhonsmovzh@users.noreply.github.com>
2024-10-25 19:49:30 +0300

    fix(Lift): лифт на двух моторах

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/lift/Lift.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/lift/Lift.kt
@@ -1,46 +1,61 @@
 package org.firstinspires.ftc.teamcode.modules.lift
 
 import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.DcMotorSimple
 import com.qualcomm.robotcore.hardware.DigitalChannel
 import org.firstinspires.ftc.teamcode.collectors.BaseCollector
 import org.firstinspires.ftc.teamcode.collectors.IRobotModule
 import org.firstinspires.ftc.teamcode.utils.configs.Configs
-import org.firstinspires.ftc.teamcode.utils.motor.Motor
 import org.firstinspires.ftc.teamcode.utils.pidRegulator.PIDRegulator
 
-object Lift: IRobotModule {
+object Lift : IRobotModule {
     var targetPosition = LiftPosition.DOWN
-    override fun update() {
-        if(targetPosition == LiftPosition.MIDDLE) {
-            motor.power = pidRegulator.update(targetPosition.position - motor.currentPosition)
-        }
-        else{
-            if(targetPosition == LiftPosition.DOWN)
-                if(endingDown.state  == false)
-                     motor.power = Configs.LiftConfig.DOWN_SPEED
-                else
-                    motor.power = Configs.LiftConfig.DOWN_SPEEDLOW
-            if(targetPosition == LiftPosition.UP)
-                if(endingUp.state  == false)
-                     motor.power = Configs.LiftConfig.UP_SPEED
-                else
-                    motor.power = Configs.LiftConfig.UP_SPEEDLOW
-            }
-    }
-    private lateinit var motor: DcMotor
-    private lateinit var endingDown: DigitalChannel
-    private lateinit var endingUp: DigitalChannel
-    private val pidRegulator = PIDRegulator(Configs.LiftConfig.LIFT_PID)
-    override fun init(collector: BaseCollector) {
-        motor = collector.devices.liftMotor
-        endingUp = collector.devices.endingUP
-        endingDown = collector.devices.endingDown
 
+    override fun update() {
+        var power = 0.0
+
+        val encLeft = _motorLeft.currentPosition.toDouble()
+        val encRight = _motorRight.currentPosition.toDouble()
+
+        if(targetPosition == LiftPosition.MIDDLE)
+            power = _posPID.update(Configs.LiftConfig.LIFT_MIDDLE_POS - (encLeft + encRight) / 2.0)
+        else if(targetPosition == LiftPosition.DOWN)
+            power = if(_endingDown.state) Configs.LiftConfig.DOWN_SPEED else Configs.LiftConfig.DOWN_SPEEDLOW
+        else
+            power = if(_endingUp.state) Configs.LiftConfig.UP_SPEED else Configs.LiftConfig.UP_SPEEDLOW
+
+        val uEnc = _syncPID.update(encLeft - encRight)
+
+        _motorLeft.power = power - uEnc
+        _motorRight.power = power + uEnc
     }
-    enum class LiftPosition(val position: Double)
-    {
-        UP(60.0),
-        DOWN(20.0),
-        MIDDLE(40.0)
+
+    private lateinit var _motorLeft: DcMotor
+    private lateinit var _motorRight: DcMotor
+
+    private lateinit var _endingDown: DigitalChannel
+    private lateinit var _endingUp: DigitalChannel
+
+    private val _posPID = PIDRegulator(Configs.LiftConfig.LIFT_PID)
+    private val _syncPID = PIDRegulator(Configs.LiftConfig.LIFT_PID_SYNC)
+
+    override fun init(collector: BaseCollector) {
+        _motorLeft = collector.devices.liftMotorLeft
+        _motorRight = collector.devices.liftMotorRight
+
+        _motorRight.direction = DcMotorSimple.Direction.REVERSE
+
+        _endingUp = collector.devices.endingUP
+        _endingDown = collector.devices.endingDown
     }
+
+    override fun start() {
+        _motorLeft.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+        _motorRight.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+
+        _motorLeft.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+        _motorRight.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+    }
+
+    enum class LiftPosition { UP, DOWN, MIDDLE }
 }
