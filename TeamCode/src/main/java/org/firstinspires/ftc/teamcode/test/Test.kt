912887aa2412141fd5cf253ac2cfa7b90c80c7cb
tikhonsmovzh <81905988+tikhonsmovzh@users.noreply.github.com>
2024-11-01 15:25:35 +0300

    проверка направления моторов

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/Test.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/Test.kt
@@ -2,16 +2,11 @@ package org.firstinspires.ftc.teamcode.test
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
-import com.qualcomm.robotcore.hardware.PwmControl
-import com.qualcomm.robotcore.hardware.Servo
-import com.qualcomm.robotcore.hardware.ServoImplEx
+import com.qualcomm.robotcore.hardware.DcMotorEx
 import com.qualcomm.robotcore.hardware.VoltageSensor
 import org.firstinspires.ftc.teamcode.collectors.BaseCollector
-import org.firstinspires.ftc.teamcode.utils.contServo.ContServo
 import org.firstinspires.ftc.teamcode.utils.devices.Battery
-import org.firstinspires.ftc.teamcode.utils.softServo.SoftServo
 import org.firstinspires.ftc.teamcode.utils.telemetry.StaticTelemetry
-import org.firstinspires.ftc.teamcode.utils.timer.Timer
 import org.firstinspires.ftc.teamcode.utils.updateListener.UpdateHandler
 
 
@@ -25,47 +20,23 @@ class Test: LinearOpMode() {
 
             val battery = Battery(hardwareMap.get(VoltageSensor::class.java, "Control Hub"))
 
-            val servRight = SoftServo(hardwareMap.get("horizontalServoRight") as ServoImplEx, 0.88)
-            val servLeft = SoftServo(hardwareMap.get("horizontalServoLeft") as ServoImplEx, 0.1)
-
-            val timer = Timer()
-
             handler.init(BaseCollector.InitContext(battery))
+
+            val rightLiftMotor = hardwareMap.get("liftMotorRight") as DcMotorEx
+            val leftLiftMotor = hardwareMap.get("liftMotorLeft") as DcMotorEx
 
             waitForStart()
             resetRuntime()
 
             handler.start()
 
-            var currentRightPos = 0.26
-            var currentLeftPos = 0.72
-
-            var a = {}
-
-            // 0.88 0.26
-            // 0.1 0.72
-
-            a = {
-                currentRightPos = 0.88
-                currentLeftPos = 0.1
-
-                timer.start({!servRight.isEnd || !servLeft.isEnd}, {
-                    currentRightPos = 0.26
-                    currentLeftPos = 0.72
-
-                    timer.start({!servRight.isEnd || !servLeft.isEnd}, a)
-                })
-            }
-
-            timer.start({!servRight.isEnd || !servLeft.isEnd}, a)
-
             while (opModeIsActive()) {
                 battery.update()
                 StaticTelemetry.update()
                 handler.update()
 
-                servRight.targetPosition = currentRightPos
-                servLeft.targetPosition = currentLeftPos
+                StaticTelemetry.addLine("leftEnc = " + leftLiftMotor.currentPosition)
+                StaticTelemetry.addLine("rightEnc = " + rightLiftMotor.currentPosition)
             }
 
             handler.stop()
