d7be9e205a70882c08f5d3e9e67bace4aa6d2cbc
tikhonsmovzh <81905988+tikhonsmovzh@users.noreply.github.com>
2024-11-29 17:10:48 +0300

    feat(ActionsRunner): выглядит рабочим

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/mainControl/actions/ActionsRunner.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/mainControl/actions/ActionsRunner.kt
@@ -3,13 +3,36 @@ package org.firstinspires.ftc.teamcode.modules.mainControl.actions
 import org.firstinspires.ftc.teamcode.collectors.BaseCollector
 import org.firstinspires.ftc.teamcode.collectors.IRobotModule
 import org.firstinspires.ftc.teamcode.collectors.events.EventBus
+import org.firstinspires.ftc.teamcode.collectors.events.IEvent
 
 class ActionsRunner: IRobotModule {
-    override fun init(
-        collector: BaseCollector,
-        bus: EventBus
-    ) {
-        TODO("Not yet implemented")
+    class RunActionsEvent(val actions: List<IAction>): IEvent
+    class RequestIsEndActionsRun(var isEnd: Boolean): IEvent
+
+    private val _actions = ArrayList<IAction>()
+
+    override fun init(collector: BaseCollector, bus: EventBus) {
+        bus.subscribe(RunActionsEvent::class){
+            if(_actions.isEmpty)
+                it.actions[0].start()
+
+            _actions.addAll(it.actions)
+        }
+
+        bus.subscribe(RequestIsEndActionsRun::class){
+            it.isEnd = _actions.isEmpty
+        }
     }
 
+    override fun update() {
+        _actions[0].update()
+
+        if(_actions[0].isEnd()) {
+            _actions[0].end()
+            _actions.removeAt(0)
+
+            if(!_actions.isEmpty)
+                _actions[0].start()
+        }
+    }
 }
