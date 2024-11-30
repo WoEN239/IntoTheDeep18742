17b5ce8db56bbdd9741c4b2322a6413968ef89c8
tikhonsmovzh <81905988+tikhonsmovzh@users.noreply.github.com>
2024-11-30 16:57:48 +0300

    fix(ActionRunner): пример для кости

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/mainControl/actions/ActionsRunner.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/mainControl/actions/ActionsRunner.kt
@@ -1,9 +1,12 @@
 package org.firstinspires.ftc.teamcode.modules.mainControl.actions
 
+import com.acmerobotics.roadrunner.Vector2d
 import org.firstinspires.ftc.teamcode.collectors.BaseCollector
 import org.firstinspires.ftc.teamcode.collectors.IRobotModule
 import org.firstinspires.ftc.teamcode.collectors.events.EventBus
 import org.firstinspires.ftc.teamcode.collectors.events.IEvent
+import org.firstinspires.ftc.teamcode.modules.mainControl.runner.TrajectorySegmentRunner
+import org.firstinspires.ftc.teamcode.utils.units.Orientation
 
 class ActionsRunner: IRobotModule {
     class RunActionsEvent(val actions: List<IAction>): IEvent
@@ -22,9 +25,30 @@ class ActionsRunner: IRobotModule {
         bus.subscribe(RequestIsEndActionsRun::class){
             it.isEnd = _actions.isEmpty()
         }
+
+        val actions = ArrayList<IAction>()
+
+        when(collector.gameSettings.startPosition){
+            BaseCollector.GameStartPosition.RED_FORWARD -> {
+                actions.add(FollowRRTrajectory(bus, TrajectorySegmentRunner.newRRTrajectory(Orientation.ZERO)
+                    .strafeTo(Vector2d(20.0, 20.0))
+
+                    .build()))
+            }
+
+            BaseCollector.GameStartPosition.RED_BACK -> TODO()
+            BaseCollector.GameStartPosition.BLUE_FORWARD -> TODO()
+            BaseCollector.GameStartPosition.BLUE_BACK -> TODO()
+            BaseCollector.GameStartPosition.NONE -> TODO()
+        }
+
+        bus.invoke(RunActionsEvent(actions))
     }
 
     override fun update() {
+        if(_actions.isEmpty)
+            return
+
         _actions[0].update()
 
         if(_actions[0].isEnd()) {
