7a41c12ad395fb617054128d6b9872f2b9b50654
tikhonsmovzh <81905988+tikhonsmovzh@users.noreply.github.com>
2025-03-05 11:31:41 +0300

    fix(TeloOp): автоинит телеопа

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/linearOpModes/TeleOpMode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/linearOpModes/TeleOpMode.kt
@@ -8,7 +8,7 @@ import org.firstinspires.ftc.teamcode.modules.mainControl.gamepad.Gamepad
 @TeleOp
 class TeleOpMode : LinearOpModeBase() {
     override fun getOpModeSettings() = OpModeSettings(
-        isAutoStart = true,
+        isAutoStart = false,
         isPreInit = false
     )
 
